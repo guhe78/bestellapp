5d77d8f16b80303e6d9143832e47eacb6a56253f
gh <gh1978@hotmail.de>
2026-01-21 18:24:11 +0100

    added template file

--- script.js
+++ script.js
@@ -8,30 +8,7 @@ function renderMenuTable(category) {
   for (let i = 0; i < category.items.length; i++) {
     let item = category.items[i];
 
-    menuItem.innerHTML += `
-    <section class="menu_container">
-              <img
-                src="${item.imageUrl}"
-                alt=""
-                class="menu_image"
-                id="menu_image"
-              />
-              <div class="menu_item_info">
-                <h4 class="menu_item_headline">
-                  <span>${item.name}</span
-                  ><span>${item.price.toFixed(2)} €</span>
-                </h4>
-                <td>${item.ingredients}</td>
-                <button
-                  class="button_addcart"
-                  id="add_cart_button"
-                  onclick='addShoppingCart(${JSON.stringify(item)})'
-                >
-                  Add to cart
-                </button>
-              </div>
-            </section>
-    `;
+    menuItem.innerHTML += getMenuTableTemplate(item);
   }
 }
 
@@ -61,7 +38,7 @@ function addShoppingCart(menu) {
       let quantity = 1;
       shoppingCart.push({ name: menu.name, price: menu.price, quantity });
     } else {
-      raiseQuantity(index);
+      increaseQuantity(index);
     }
   }
 
@@ -93,19 +70,7 @@ function renderShoppingCart() {
   shoppingCartContainer.innerHTML = "";
 
   for (let i = 0; i < shoppingCart.length; i++) {
-    shoppingCartContainer.innerHTML += `
-     <div class="shopping_cart_item">
-        <p>${shoppingCart[i].quantity}x ${shoppingCart[i].name}</p>
-        <div class="buttons_price_container">
-          <span class="amount_buttons_bill">
-            <img src="./assets/images/page_content/buttons/trash.png" alt="" onclick="deleteShoppingCartItem(${i})" />
-            <button onclick="decreaseQuantity(${[i]})">-1</button>
-            <button onclick="increaseQuantity(${[i]})">+1</button>
-          </span>
-          <p>${(shoppingCart[i].quantity * shoppingCart[i].price).toFixed(2)} €</p>
-        </div>
-      </div>
-    `;
+    shoppingCartContainer.innerHTML += getShoppingCartTemplate();
   }
 
   renderBill();
@@ -127,16 +92,5 @@ function renderBill() {
 
   totalPrice = price + fee;
 
-  billContainer.innerHTML = `
-    <h4>
-      <span>Subtotal</span> <span>${price.toFixed(2)} €</span>
-    </h4>
-    <h4>
-      <span>Delivery fee</span> <span>${fee.toFixed(2)} €</span>
-    </h4>
-    <hr />
-    <h3>
-      <span>Total</span> <span>${totalPrice.toFixed(2)} €</span>
-    </h3>
-    <button>Buy now (${totalPrice.toFixed(2)} €)</button>`;
+  billContainer.innerHTML = getBillTemplate(price, totalPrice, fee);
 }
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,1 +1,57 @@
-function getBillTemplate() {}
+function getBillTemplate(price, totalPrice, fee) {
+  return `
+    <h4>
+      <span>Subtotal</span> <span>${price.toFixed(2)} €</span>
+    </h4>
+    <h4>
+      <span>Delivery fee</span> <span>${fee.toFixed(2)} €</span>
+    </h4>
+    <hr />
+    <h3>
+      <span>Total</span> <span>${totalPrice.toFixed(2)} €</span>
+    </h3>
+    <button>Buy now (${totalPrice.toFixed(2)} €)</button>`;
+}
+
+function getMenuTableTemplate(item) {
+  return `
+    <section class="menu_container">
+              <img
+                src="${item.imageUrl}"
+                alt=""
+                class="menu_image"
+                id="menu_image"
+              />
+              <div class="menu_item_info">
+                <h4 class="menu_item_headline">
+                  <span>${item.name}</span
+                  ><span>${item.price.toFixed(2)} €</span>
+                </h4>
+                <td>${item.ingredients}</td>
+                <button
+                  class="button_addcart"
+                  id="add_cart_button"
+                  onclick='addShoppingCart(${JSON.stringify(item)})'
+                >
+                  Add to cart
+                </button>
+              </div>
+            </section>
+    `;
+}
+
+function getShoppingCartTemplate() {
+  return `
+     <div class="shopping_cart_item">
+        <p>${shoppingCart[i].quantity}x ${shoppingCart[i].name}</p>
+        <div class="buttons_price_container">
+          <span class="amount_buttons_bill">
+            <img src="./assets/images/page_content/buttons/trash.png" alt="" onclick="deleteShoppingCartItem(${i})" />
+            <button onclick="decreaseQuantity(${[i]})">-1</button>
+            <button onclick="increaseQuantity(${[i]})">+1</button>
+          </span>
+          <p>${(shoppingCart[i].quantity * shoppingCart[i].price).toFixed(2)} €</p>
+        </div>
+      </div>
+    `;
+}
